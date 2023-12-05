a2bdcfca54182d38f9657ce3c53835cf0ed6e13e
Yana Kond <https://github.com/YanaKond>
2023-12-05 12:53:05 +0300

    add example 1

--- Example01/Program.cs
+++ Example01/Program.cs
@@ -1,1 +1,47 @@
-﻿// kzkz
+﻿// Задача 1: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+void InputMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(0, 11);
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write(matrix[i, j] + "\t");
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    bool found = false;
+    Console.WriteLine("Введите позицию элемента");
+    int [] position = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i==position[0] && j==position[1])
+            {
+                Console.WriteLine(matrix[i, j]);
+                found = true;
+            }
+        }
+    }
+    if (found==false)
+        Console.WriteLine("Такого элемента нет");
+}
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+ReleaseMatrix(matrix);
